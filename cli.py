7ba23242258dec4c0b28b45e48f731d9bf6d7d29
Dylan McDougall <dylanalexandermcdougall@gmail.com>
2022-10-05 12:08:53 -0400

    Modify to match design doc

--- cli.py
+++ cli.py
@@ -2,17 +2,47 @@ from sys import argv
 import start
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     _argv = argv[1:] if argv[0].startswith('python') else argv
+    print(_argv)
 
-    if len(_argv) < 2:
-        raise RuntimeError('No container name provided')
+    subcmd = _argv[1]
 
-    elif _argv[1][0] == "-":
+    if subcmd.lower() in ('interact', 'x'):
+        start.start(_argv[2])
+
+    elif subcmd.lower() in ('start', 's'):
         raise NotImplementedError()
-    
-    elif len(argv) > 2:
+
+    elif subcmd.lower() in ('stop', 'h'):
         raise NotImplementedError()
-    
-    else:
-        start.start(_argv[1])
+
+    elif subcmd.lower() in ('install', 'i'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('download', 'w'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('acrhive', 'a'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('create', 'c'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('delete', 'd'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('send-file'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('get-file'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('add-repo'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('remove-repo'):
+        raise NotImplementedError()
+
+    elif subcmd.lower() in ('update-repo'):
+        raise NotImplementedError()
